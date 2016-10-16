dce6414bdf89ae5db863b923c5bf5718cc4359dd
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-10-16 14:28:35 -0500

    Server setup

---
+++ server/server.js
@@ -0,0 +1,83 @@
+'use strict';
+
+
+/*------------------------------------*\
+  #DEPENDENCIES
+\*------------------------------------*/
+
+var express = require( `express` );
+var mongoose = require( `mongoose` );
+var bodyParser = require( `body-parser` );
+var passport = require( `passport` );
+var session = require( `express-session` );
+var config = require( `./config.js` );
+
+
+
+
+
+/*------------------------------------*\
+  #APP
+\*------------------------------------*/
+
+var app = express();
+
+app.use( express.static( `./public` )) ;
+app.use( express.static( `${__dirname}/../node_modules` ));
+app.use( bodyParser.json( { limit: '50mb' } ) );
+app.use( bodyParser.urlencoded( { limit: '50mb', extended: true } ) );
+app.use( session( config.session ) );
+app.use( passport.initialize() );
+app.use( passport.session() );
+
+
+
+
+
+/*------------------------------------*\
+  #AUTH SETUP
+\*------------------------------------*/
+
+
+
+
+
+
+
+/*------------------------------------*\
+  #PASSPORT
+\*------------------------------------*/
+
+passport.serializeUser( function( user, done ) {
+  done( null, user._id );
+} );
+passport.deserializeUser( function( _id, done ) {
+  User.findById( _id, function( err, user ) {
+    done( err, user );
+  } );
+} );
+
+
+
+
+
+/*------------------------------------*\
+  #DATABASE
+\*------------------------------------*/
+
+mongoose.connect( config.mongo.localmongoURI, function ( err, res ) {
+	if ( err ) console.log( 'Error connecting to database' )
+	else console.log( 'Yente database now connected!' )
+} );
+
+
+
+
+
+/*------------------------------------*\
+  #LISTEN
+\*------------------------------------*/
+
+app.listen( config.port, function() {
+	console.log( 'Pulp Express server listening on port', config.port );
+} );
