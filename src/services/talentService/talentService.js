d35db1cd39a05b635f4fdc48f19f1aeaf690ca93
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-10-18 20:00:15 -0500

    formatting users for menu

--- src/services/talentService/talentService.js
+++ src/services/talentService/talentService.js
@@ -15,6 +15,7 @@
 
 		return {
 			getAllTalent: getAllTalent
+			// TEMPgetAllTalent: TEMPgetAllTalent
 		};
 
 
@@ -26,17 +27,50 @@
 			All detailed logic(function definitions) goes below this comment.
 		\**************************************************************************/
 
+		// function getAllTalent() {
+		// 	return $q.when( menuItems );
+		// }
+
 		function getAllTalent() {
-			var menuItems = {};
+			var menuItems = [];
 			return $http.get( '/api/talent' )
 				.then( function( talent ) {
 					var talentData = talent.data;
-					for( var i = 0; i < talentData.lenth; i++ ) {
-						if( menuItems.hasOwnProperty( talentData[i].title ) ) {
-							
+					for( var i = 0; i < talentData.length; i++ ) {
+						if( talentData[i].department.length === 1 ) {
+							var yup = deptExists( talentData[ i ].department[ 0 ], 'title', menuItems );
+							pushTalentToMenu( yup, menuItems, talentData, i );
+						} else {
+
 						}
 					}
+					return menuItems;
 				} );
+		}
+
+		/**************************************************************************\
+			Helper functions
+		\**************************************************************************/
+
+		function deptExists( dept, prop, list ) {
+			var i;
+			for ( i = 0; i < list.length; i++ ) {
+				if ( list[ i ][ prop ] === dept ) {
+					return [ true, i ];
+				}
+			}
+			return [ false ];
+		}
+
+		function pushTalentToMenu( arr, list, talent, index ) {
+			if( arr[ 0 ] ) {
+				list[ arr[ 1 ] ].talent.push( { name: talent[ index ].name } );
+			} else {
+				list.push( {
+					title: talent[ index ].department[ 0 ],
+					talent: [ { name: talent[ index ].name } ]
+				} );
+			}
 		}
 
 	}
