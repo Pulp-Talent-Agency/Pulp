d07fe098f0963bd358a0110fc8809ee938fb7656
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-10-22 11:18:36 -0500

    added put and delete routes

--- server/features/talent/talentRoutes.js
+++ server/features/talent/talentRoutes.js
@@ -2,7 +2,10 @@ const talentCtrl = require( './talentCtrl.js' );
 
 module.exports = function( app ) {
 
-	app.post( '/api/talent', talentCtrl.postTalent );
+	app.post( '/api/talent', talentCtrl.postNewTalent );
 	app.get( '/api/talent', talentCtrl.getAllTalent );
+
+	app.put( '/api/talent/:id', talentCtrl.updateExistingTalent );
+	app.delete( '/api/talent/:id', talentCtrl.deleteExistingTalent );
 
 }
