4e752a5f959515d3ae995e4e68ee821e4f0b382a
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-10-27 19:10:49 -0500

    switching over to not using jquery

--- src/services/navService/navService.js
+++ src/services/navService/navService.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-
 navService.$inject = [];
 
 function navService() {
@@ -22,24 +20,16 @@ function navService() {
 		var $sideNav = document.getElementsByClassName( 'sidenav' )[ 0 ];
 
 		if( $sideNav.style.left === '0rem' ) {
-			move( $sideNav, 'left', 0, -50, 'rem' );
+			moveAnimation( $sideNav, 'left', 0, -50, 'rem' );
 		}
 		else {
-			move( $sideNav, 'left', -50, 0, 'rem' );
+			moveAnimation( $sideNav, 'left', -50, 0, 'rem' );
 		}
 		closeOthers( menuItems );
 	}
 
 	function toggleHeaderSubNav( menuItems ) {
-		var $headerNav = $( '.headernav' );
 
-		if( $headerNav.css( 'left' ) === '0px' ) {
-			$headerNav.animate( { left: '-100rem' }, 200, 'swing' );
-		}
-		else {
-			$headerNav.animate( { left: '0' }, 200, 'swing' );
-		}
-		closeOthers( menuItems );
 	}
 
 	function closeOthers( menuItems ) {
@@ -58,7 +48,7 @@ function navService() {
 		# Helper functions
 	\**************************************************************************/
 
-	function move( elem, prop, startValue, endValue, unit ) {
+	function moveAnimation( elem, prop, startValue, endValue, unit ) {
 
     function frame() {
 			if( startValue < endValue ) {
