60bc0b6bdab1c08b98a37821d0dd6975e8efe845
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-11-08 13:36:44 -0600

    ready for lecture

--- server/features/photo/photoCtrl.js
+++ server/features/photo/photoCtrl.js
@@ -1,16 +1,44 @@
 const Photo = require( './Photo.js' );
-const Talent = require( './Talent.js' );
+const AWS = require( 'aws-sdk' );
+const config = require( './../../config.js' );
+
+AWS.config.update({
+  accessKeyId: config.amazonS3.accessKeyId,
+	secretAccessKey: config.amazonS3.secretAccessKey,
+	region: config.amazonS3.region
+});
+
+const s3 = new AWS.S3();
 
 module.exports = {
 
 	postNewPhoto: function( req, res ) {
-		Photo.create( req.body, function( err, photo ) {
-			if( err ) {
-				return res.status( 500 ).json( err );
-			} else {
-				return res.status( 200 ).json( photo );
-			}
-		} );
+		const buf = new Buffer(req.body.imageBody.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+
+	  // bucketName var below crates a "folder" for each user
+	  var bucketName = 'pulp-lecture/' + req.body.userEmail;
+	  var params = {
+      Bucket: bucketName,
+			Key: req.body.imageName,
+			Body: buf,
+			ContentType: 'image/' + req.body.imageExtension,
+			ACL: 'public-read'
+	  };
+
+	  s3.upload(params, function (err, s3data) {
+	    if (err) {
+				return res.status(500).send(err);
+			};
+			Photo.create( { amazonS3: s3data }, function( error, photo ) {
+				if ( 'Pulp', error ) {
+					return res.status( 500 ).send( error );
+				}
+				else {
+					return res.status( 200 ).json( photo );
+				}
+			} );
+
+	  });
 	},
 
 	getAllPhotos: function( req, res ) {
